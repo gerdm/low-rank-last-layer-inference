5769d6470b3139ed0c4cdabc48e55fe23eef8cc9
Gerardo Duran-Martin <g.duran@me.com>
2025-04-14 14:08:40 +0000

    feat: mnist-bandit experiment

---
+++ experiments/agents_mnist.py
@@ -0,0 +1,145 @@
+import jax
+import optax
+import jax.numpy as jnp
+import flax.linen as nn
+from functools import partial
+from vbll_fifo import FifoLaplaceDiag
+from rebayes_mini.methods import low_rank_filter as lofi 
+from rebayes_mini.methods import low_rank_last_layer as flores
+from rebayes_mini.methods import low_rank_filter_revised as lrkf
+
+class CNN(nn.Module):
+    num_actions: int = 10
+
+    @nn.compact
+    def __call__(self, x):
+        x = x if len(x.shape) > 3 else x[None, :]
+        x = nn.Conv(features=6, kernel_size=(5, 5))(x)
+        x = nn.elu(x)
+        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
+        x = nn.Conv(features=16, kernel_size=(5, 5), padding="VALID")(x)
+        x = nn.elu(x)
+        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
+        x = x.reshape((x.shape[0], -1))  # Flatten
+        x = nn.Dense(features=120)(x)
+        x = nn.elu(x)
+        x = nn.Dense(features=84)(x)
+        x = nn.elu(x)
+        x = nn.Dense(self.num_actions, name="last_layer")(x)
+        return x.squeeze()
+
+
+def mean_fn(params, x, model):
+    x = jnp.atleast_2d(x)
+    n_in = len(x[0].ravel())
+    if n_in == 28 ** 2 + 1:
+        # Passing context and action
+        action = x[:, 0].astype(int)
+        x = x[:, 1:].reshape(-1, 28, 28, 1)
+        eta = model.apply(params, x)
+        mean = jax.nn.sigmoid(eta[action])
+        return jnp.atleast_1d(mean)
+    elif n_in == 28 ** 2:
+        # Passing context only
+        x = x.reshape(-1, 28, 28, 1)
+        eta = model.apply(params, x)
+        return eta
+    else:
+        raise ValueError("Undefined input dimension")
+
+
+def cov_fn(eta, eps=0.1):
+    mean = jax.nn.softmax(jnp.atleast_1d(eta))
+    return jnp.diag(mean) - jnp.outer(mean, mean) + jnp.eye(len(mean)) * eps
+
+
+def lossfn(params, counter, x, y, apply_fn, noise=0.1):
+    x = jnp.atleast_2d(x)
+    action = x[:, 0].astype(int)
+    x = x[:, 1:].reshape(-1, 28, 28, 1)
+    res = apply_fn(params, x)
+
+    # params_flat, _ = ravel_pytree(params)
+    
+    yhat = jnp.take_along_axis(jnp.atleast_2d(res), action[:, None], axis=-1).squeeze()
+    yhat = jax.nn.sigmoid(yhat)
+    y = y.squeeze()
+
+    log_likelihood = y * jnp.log(yhat) + (1 - y) * jnp.log1p(-yhat)
+    log_likelihood = (log_likelihood * counter).sum() / counter.sum()
+    return -log_likelihood #+ 0.1 * jnp.power(params_flat, 2).mean()
+
+
+### Defining agents ###
+model = CNN(num_actions=10)
+
+def agent_ogd():
+    buffer_size = 1
+    n_inner = 5
+    learning_rate = 1e-4
+    agent = FifoLaplaceDiag(
+        partial(mean_fn, model=model),
+        lossfn,
+        tx=optax.adamw(learning_rate),
+        buffer_size=buffer_size,
+        dim_features = 28 ** 2 + 1,
+        dim_output=1,
+        n_inner=n_inner,
+    )
+    return agent, {}
+
+
+def agent_flores():
+    agent = flores.LowRankLastLayer(
+        partial(mean_fn, model=model),
+        cov_fn,
+        rank=50,
+        dynamics_hidden=1e-3,
+        dynamics_last=1e-3,
+    )
+    init_params = {
+        "low_rank_diag": True,
+        "cov_hidden": 1.0,
+        "cov_last": 1.0
+    }
+    return agent, init_params
+
+
+def agent_lrkf():
+    agent = lrkf.LowRankCovarianceFilter(
+        partial(mean_fn, model=model),
+        cov_fn,
+        rank=50,
+        dynamics_covariance=1e-3,
+    )
+
+    init_params = {
+        "low_rank_diag": True,
+        "cov": 1.0
+    }
+
+    return agent, init_params
+
+
+def agent_lofi():
+    dynamics = 1e-3
+    rank = 50
+    agent = lofi.LowRankPrecisionFilter(
+        partial(mean_fn, model=model),
+        cov_fn,
+        dynamics_covariance=dynamics,
+        rank=rank
+    )
+    init_params = {
+        "cov": 1.0
+    }
+    return agent, init_params
+
+
+
+agents = {
+    "LRKF": agent_lrkf,
+    "FLoRES": agent_flores,
+    "OGD": agent_ogd,
+    "LoFi": agent_lofi
+}
--- experiments/run_mnist_bandit.py
+++ experiments/run_mnist_bandit.py
@@ -1,38 +1,17 @@
+import os
 import jax
+import click
+import pickle
 import gymnax
-import flax.linen as nn
+import numpy as np
 import jax.numpy as jnp
+from time import time
 from functools import partial
-from rebayes_mini.methods import low_rank_last_layer as flores
-from rebayes_mini.methods import low_rank_filter as lofi
+from agents_mnist import agents, model
 
 
-class CNN(nn.Module):
-    num_actions: int = 10
-
-    @nn.compact
-    def __call__(self, x):
-        x = x if len(x.shape) > 3 else x[None, :]
-        x = nn.Conv(features=6, kernel_size=(5, 5))(x)
-        x = nn.elu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
-        x = nn.Conv(features=16, kernel_size=(5, 5), padding="VALID")(x)
-        x = nn.elu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
-        x = x.reshape((x.shape[0], -1))  # Flatten
-        x = nn.Dense(features=120)(x)
-        x = nn.elu(x)
-        x = nn.Dense(features=84)(x)
-        x = nn.elu(x)
-        x = nn.Dense(self.num_actions, name="last_layer")(x)
-        return x.squeeze()
-
-
-def setup(key: int):
-    model = CNN(num_actions=10)
-    key = jax.random.PRNGKey(key)
-    key_env, key_env_step, key_act, key_init_params, key_run = jax.random.split(key, 5)
-    params_init = model.init(key_init_params, jnp.ones((28, 28, 1)))
+def setup_env(key):
+    key_env, key_env_step, key_act, key_run = jax.random.split(key, 4)
 
     # Instantiate the environment & its settings.
     env, env_params = gymnax.make("MNISTBandit-bsuite")
@@ -40,23 +19,51 @@ def setup(key: int):
     # Reset the environment.
     context, state = env.reset(key_env, env_params)
 
-    # Initialise agent
-    params_init = model.init(key_init_params, context[..., None])
-
     # Sample a random action.
     action = env.action_space(env_params).sample(key_act)
 
     context_init, state_init, _, _, _ = env.step(key_env_step, state, action, env_params)
 
     env_stuff = (env, context_init, state_init, env_params)
-    model_stuff = (model, params_init)
 
-    return env_stuff, model_stuff, key_run
-
+    return env_stuff, key_run
 
 
-def step_ts(state, t, agent, env_params):
-    key_step = jax.random.fold_in(key_run, t)
+def step_egreedy(state, t, agent, key_base, env, env_params, eps):
+    """
+    Step for epsilon-greedy agent
+    """
+    key_step = jax.random.fold_in(key_base, t)
+    key_step, key_take = jax.random.split(key_step)
+    
+    bel, context, env_state = state
+
+    # Take action
+    key_eps, key_take = jax.random.split(key_take)
+    yhat = agent.predict_fn(bel, context.ravel())
+    action = yhat.argmax()
+    take_random = jax.random.bernoulli(key_eps, p=eps)
+    action_random = jax.random.choice(key_take, 10)
+
+    action = action * (1 - take_random) + action_random * take_random
+    action = action.astype(int)
+    
+    # Obtain reward
+    y = jax.nn.one_hot(env_state.correct_label, 10)[action]
+
+    # Update belief
+    # X = (action, context[..., None])
+    X = jnp.concat([jnp.array([action]), context.ravel()])
+    bel_update = agent.update(bel, y, X)
+    
+    # Take next step
+    context_new, env_state, reward, done, _ = env.step(key_step, env_state, action, env_params)
+
+    return (bel_update, context_new, env_state), (action, reward)
+
+
+def step_ts(state, t, agent, key_base, env, env_params):
+    key_step = jax.random.fold_in(key_base, t)
     key_step, key_take = jax.random.split(key_step)
     
     bel, context, env_state = state
@@ -78,56 +85,62 @@ def step_ts(state, t, agent, env_params):
     return (bel_update, context_new, env_state), (action, reward)
 
 
-(env, context_init, state_init, env_params), (model, params_init), key_run = setup(314)
+def run_agent(
+    key, agent, bel_init, n_steps, step_fn, step_fn_kwargs
+):
+    (env, context_init, state_init, env_params), key_run = setup_env(key)
 
-def mean_fn(params, x):
-    if isinstance(x, tuple):
-        action = x[0]
-        x = x[1]
-        eta = model.apply(params, x)
-        mean = jax.nn.sigmoid(eta[action])
-        return jnp.atleast_1d(mean)
-    else:
-        eta = model.apply(params, x)
-        return eta
-
-
-def cov_fn(eta, eps=0.1):
-    mean = jax.nn.softmax(jnp.atleast_1d(eta))
-    return jnp.diag(mean) - jnp.outer(mean, mean) + jnp.eye(len(mean)) * eps
-
-
-def run_agent_ts(bel_init, context_init, state_init, env_params, n_steps):
     u_init = (bel_init, context_init, state_init)
-    _step = partial(step_ts, agent=agent, env_params=env_params)
+    _step = partial(step_fn, agent=agent, key_base=key_run, env=env, env_params=env_params, **step_fn_kwargs)
     (bel_final, *_), (actions, rewards) = jax.lax.scan(_step, u_init, jnp.arange(n_steps))
     rewards = jax.block_until_ready((rewards + 1) / 2)
-    return actions, rewards
+    return bel_final, actions, rewards
+
+@partial(jax.vmap, in_axes=(0, None, None, None, None, None))
+def run_agents(
+    key, agent, bel_init, n_steps, step_fn, step_fn_kwargs
+):
+    bel_final, action, rewards = run_agent(key, agent, bel_init, n_steps, step_fn, step_fn_kwargs)
+    return action, rewards
 
 
-num_steps = 20_000
 
-## Flores agent
-print("Running Flores agent")
-agent = flores.LowRankLastLayer(
-    mean_fn,
-    cov_fn,
-    rank=50,
-    dynamics_hidden=0.0,
-    dynamics_last=0.0
-)
-bel_init = agent.init_bel(params_init, low_rank_diag=True, cov_hidden=1.0, cov_last=1.0)
-actions, rewards = run_agent_ts(bel_init, context_init, state_init, env_params, num_steps)
-print(rewards.sum())
+@click.command()
+@click.option("--agent", help="Agent to run")
+@click.option("--key", default=314, help="Random key")
+@click.option("--eps", default=0.05, help="Epsilon value")
+@click.option("--num_steps", default=40_000, help="Total number of observations")
+@click.option("--base_path", default=".", help="Base path for saving results")
+@click.option("--num_trials", default=1, help="Number of simulations ot run")
+def run_experiment(agent, key, eps, num_steps, base_path, num_trials):
+    print(f"Running {agent} agent")
+    key = jax.random.PRNGKey(key)
+    key_params, key_run = jax.random.split(key)
+    params_init = model.init(key_params, jnp.ones((28, 28, 1)))
+
+    agent_instance, init_kwargs = agents[agent]()
+    bel_init = agent_instance.init_bel(params_init, **init_kwargs)
+
+    step_fn_config = {"eps": eps}
+    keys_run = jax.random.split(key_run, num_trials)
+    time_init = time()
+    actions, rewards = run_agents(keys_run, agent_instance, bel_init, num_steps, step_egreedy, step_fn_config)
+    res = {
+        "actions": actions,
+        "rewards": rewards,
+        "eps": eps,
+    }
+    res = jax.tree.map(np.array, res)
+    time_end = time()
+    res = {"time": time_end - time_init, **res}
+
+    filename = f"{agent}_eps_{eps * 100:.0f}.pkl"
+    path_out = os.path.join(base_path, filename)
+    with open(path_out, "wb") as f:
+        pickle.dump(res, f)
+    print(f"Results saved to {path_out}")
+    print(f"Total time: {time_end - time_init:.4f} seconds")
 
 
-## LoFi agent
-print("Running LoFi agent")
-dynamics = 1e-3
-agent = lofi.LowRankPrecisionFilter(
-    mean_fn, cov_fn, dynamics_covariance=dynamics, rank=50
-)
-
-bel_init = agent.init_bel(params_init, cov=1.0)
-actions, rewards = run_agent_ts(bel_init, context_init, state_init, env_params, num_steps)
-print(rewards.sum())
+if __name__ == "__main__":
+    run_experiment()
