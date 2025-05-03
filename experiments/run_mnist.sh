b6567e3b611c1088d1cd3f659e1a8d798cd80b6d
Gerardo Duran-Martin <g.duran@me.com>
2025-05-03 07:03:56 +0000

    refactor: experiments

--- experiments/run_mnist.sh
+++ experiments/run_mnist.sh
@@ -4,28 +4,28 @@ export key=314
 
 ## Epsilon Greedy
 
-# python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent FLoRES --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent FLoRES --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
 
 python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent FLoRESLite --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent LRKF --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent LRKF --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps --key $key
 
 ## Thompson Sampling
 
-# python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent FLoRES --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent FLoRES --num_steps $num_steps --key $key
 
 python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent FLoRESLite --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent LRKF --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent LRKF --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
 
-# python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps  --key $key
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps  --key $key
