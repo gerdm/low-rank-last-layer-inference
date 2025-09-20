53de8e645149d7eddcd3610b33310d57a51b1c71
Gerardo Duran-Martin <g.duran@me.com>
2025-09-20 13:57:08 +0000

    feat: run_mnist_refactor

--- experiments/run_mnist_refactor.sh
+++ experiments/run_mnist_refactor.sh
@@ -2,13 +2,38 @@ export num_steps=40000
 export num_trials=10
 export key=314
 
-# Laplace
-python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
-python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
+## Epsilon Greedy
 
-## Lofi
-python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent FLoRES --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent FLoRESLite --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent LRKF --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-epsilon-greedy --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps --key $key
+
+# ## Thompson Sampling
+
 python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
 
-## HiLoFi
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-ts --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps  --key $key
+
+## Predictive Bayes
+
 python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent FLoRES --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent FLoRESLite --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent LoFi --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent LRKF --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent adamw --num_steps $num_steps --key $key
+
+python -W ignore run_mnist_bandit.py run-predictive-bayes --base_path output --num_trials $num_trials --agent muon --num_steps $num_steps  --key $key
